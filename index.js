8908c5977bd5a3932bb240109618822d36c00bb4
Sandro <sandro.pischinger@gmail.com>
2019-01-28 23:41:20 +0100

    refactor + add find command

--- index.js
+++ index.js
@@ -4,28 +4,40 @@ const packetfinder = require('./src/packetfinder');
 const hexRegex = /^[0-9a-fA-F]{3,}$/;
 
 
-rl.setPrompt('hex data> ');
+rl.setPrompt('>> ');
 rl.prompt();
-rl.on('line', function(line) {
+rl.on('line', function (line) {
     if (line === 'quit' || line === 'exit' || line === 'bye') rl.close();
     console.log();
-    if (line === 'find') {
+    if (line.startsWith('find')) {
         let args = line.normalize().split(' ');
         if (args.length < 2) {
             console.log('Usage: find [packetID]');
         } else {
             let packetID = args[1];
+            if (isNaN(packetID)) {
+                console.log("Input is not a number!");
+            } else {
+                let number = parseInt(packetID);
+                packetfinder.printStructure(number);
+            }
+            
         }
-    }
-    else if (hexRegex.test(line)) {
-        packetfinder.dump(line, function(message, answer) {
-          rl.question(message, answer);
-        });
+    } else if (line.startsWith('help')) {
+        console.log(
+            `Help:\n  - help            : show this help\n  - exit            : exit this app\n  - find [packetID] : find a packet by ID\n  - [hex]           : decode a packet by hex data
+              `);
+        
+    } else if (hexRegex.test(line)) {
+        packetfinder.dump(line);
     } else {
-      console.log('Input is not a valid hex stream.\n');
+        console.log('Enter a valid hex stream. Write \'help\' for help\n');
     }
 
     rl.prompt();
-}).on('close',function(){
+}).on('close', function () {
     process.exit(0);
 });
+
+
+module.exports.readLine = rl;
--- src/packetfinder.js
+++ src/packetfinder.js
@@ -15,7 +15,7 @@ module.exports.bound = {
   SERVERBOUND: "serverbound"
 }
 
-var PacketInfo = class PacketInfo {
+let PacketInfo = class PacketInfo {
   constructor(type, bound, id, name, fields) {
     this.type = type;
     this.bound = bound;
@@ -23,13 +23,60 @@ var PacketInfo = class PacketInfo {
     this.name = name;
     this.fields = fields;
   }
+  display() {
+    console.log(
+      `${this.name} Packet:
+      Type: ${this.type}
+      Bound: ${this.bound}
+
+      PacketID: ${this.id}
+      `);
+    this.fields.forEach(function (entry) {
+      console.log(`      ${entry.name}: ${entry.value}`);
+    })
+
+  }
+}
+
+function getPacketInfo(node, packetId) {
+    let path = node.path;
+    
+    let packet = node.value;
+    let name = packet.name;
+    let fields = packet.fields;
+
+    let type = path[1];
+    let bound = path[2];
+
+    return new PacketInfo(type, bound, packetId, name, fields);
 }
 
 module.exports.findStructure = function (packetId) {
-  return jp.nodes(data, `$..${"id"+packetId.value}`);
+  let nodes = jp.nodes(data, `$..packet[?(@.id=='${this.toHexString(packetId)}')]`);
+  return nodes;
 }
 
-module.exports.dump = function(hex, ask) {
+module.exports.printStructure = function(packetId) {
+  let nodes = this.findStructure(packetId);
+  
+  let packetInfos = [];
+
+  for (var i = 0; i < nodes.length; i++) {
+    packetInfos[i] = getPacketInfo(nodes[i], packetId);
+  }
+
+  if (packetInfos.length < 1) {
+    console.log(
+      `No Packet found for id ${packetId}`);
+  } else if (packetInfos.length === 1) {
+    packetInfos[0].display();
+  } else {
+    pickStructure(packetInfos);
+  }
+
+}
+
+module.exports.dump = function(hex) {
   let pr = new PacketReader(hex);
 
   let length = pr.readVarint();
@@ -45,26 +92,18 @@ module.exports.dump = function(hex, ask) {
 
   let packetId = pr.readVarint();
   pr.setResetPoint();
-  this.find(length, packetId, pr, ask);
+  this.find(length, packetId, pr);
 
 }
 
-module.exports.find = function(length, packetId, packetReader, ask) {
-  let nodes = jp.nodes(data, `$..${"id"+packetId.value}`);
+module.exports.find = function(length, packetId, packetReader) {
+  let nodes = jp.nodes(data, `$..['${this.toHexString(packetId.value)}']`);
   let validDecoders = [];
 
   for (var i = 0; i < nodes.length; i++) {
     let node = nodes[i];
-    let path = node.path;
 
-    let packet = node.value;
-    let name = packet.name;
-    let fields = packet.fields;
-
-    let type = path[1];
-    let bound = path[2];
-
-    let packetInfo = new PacketInfo(type, bound, packetId, name, fields);
+    let packetInfo = getPacketInfo(node, packetId.value);
 
     let decoder = new Decoder(length, packetId, packetReader, packetInfo);
 
@@ -83,11 +122,12 @@ module.exports.find = function(length, packetId, packetReader, ask) {
   } else if (validDecoders.length === 1) {
     validDecoders[0].display();
   } else {
-    pickDecoder(validDecoders, ask);
+    pickDecoder(validDecoders);
   }
 }
 
-function pickDecoder(decoders, ask) {
+
+function pickDecoder(decoders) {
   var pickmessage = ["Pick one of the following decoders:\n"];
   for (var i = 0; i < decoders.length; i++) {
     var decoder = decoders[i];
@@ -95,7 +135,8 @@ function pickDecoder(decoders, ask) {
   }
   pickmessage.push("\n")
   pickmessage = pickmessage.join("\n");
-  ask(pickmessage, function(answer) {
+  pickmessage += "pick>";
+  index.readLine.question(pickmessage, function(answer) {
     if (isNaN(answer)) {
       console.log(`The input must be a number.`);
       return;
@@ -105,6 +146,38 @@ function pickDecoder(decoders, ask) {
       return;
     }
     decoders[answer-1].display();
-
+    index.readLine.prompt();
   });
+}
+
+function pickStructure(structures) {
+  var pickmessage = ["Pick one of the following structures:\n"];
+  for (var i = 0; i < structures.length; i++) {
+    var structure = structures[i];
+    pickmessage[i + 1] = `  ${i+1}) ${structure.name} (type=${structure.type}, bound=${structure.bound})`;
+  }
+  pickmessage.push("\n")
+  pickmessage = pickmessage.join("\n");
+  pickmessage += "pick>";
+  index.readLine.question(pickmessage, function(answer) {
+    if (isNaN(answer)) {
+      console.log(`The input must be a number.`);
+      return;
+    }
+    if (answer < 0 || answer > structures.length) {
+      console.log(`The input must be between 1 and ${structures.length}`);
+      return;
+    }
+    structures[answer-1].display();
+    index.readLine.prompt();
+  });
+}
+
+module.exports.toHexString = function (number) {
+  if (isNaN(number)) throw "Input is not a number";
+  hexString = number.toString(16);
+  if (hexString.length % 2) {
+    hexString = '0' + hexString;
+  }
+  return '0x' + hexString;
 }
